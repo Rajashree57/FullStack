225b637d95de03d5b88588c68853ed5ebcaf5326
Rajashree57 <msrajashree1@gmail.com>
2023-02-02 01:02:49 +0530

    okay

---
+++ javascript/JavaScript/Arrays/array.js
@@ -0,0 +1,21 @@
+// empty array
+const myList = [ ];
+console.log(myList);
+
+// array of numbers
+const numberArray = [ 2, 4, 6, 8];
+
+// array of strings
+const stringArray = [ 'eat', 'work', 'sleep'];
+
+// array with mixed data types
+const newData = ['work', 'exercise', 1, true];
+
+//runcode
+const myArray = ['h', 'e', 'l', 'l', 'o'];
+
+// first element
+console.log(myArray[0]);  // "h"
+
+// second element
+console.log(myArray[1]); // "e"
