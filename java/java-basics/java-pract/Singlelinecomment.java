11a9214fc8bfa343d428dccb970e0d4b4a904a97
Rajashree57 <msrajashree1@gmail.com>
2023-02-21 16:00:12 +0530

    okay

---
+++ java/java-basics/java-pract/Singlelinecomment.java
@@ -0,0 +1,7 @@
+package syntax1;
+public class Singlelinecomment {
+    public static void main(String[] args) {
+        //This is a single line comment
+        System.out.println("Hello World!!!");
+      }
+}
