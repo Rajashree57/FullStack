01d1d3b2aee8ebd638f37aa6f59952304c379207
Rajashree57 <msrajashree1@gmail.com>
2023-02-21 16:03:38 +0530

    ok

---
+++ java/java-basics/java-pract/Multilinecomment.java
@@ -0,0 +1,13 @@
+package syntax1;
+
+public class Multilinecomment {
+    public static void main(String[] args) {
+        /*This
+         * is 
+         * a
+         * Multiline 
+         * Comment
+         */
+        System.out.println("Hello World!!!");
+      }
+}
