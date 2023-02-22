4bc3411ada350afebb9e41405e580941cc7d41b3
Rajashree57 <msrajashree1@gmail.com>
2023-02-22 13:05:01 +0530

    okie

---
+++ java/java-basics/DataTypes/Operators/ScannerExamples/ConditionalStatements/Nestedif.java
@@ -0,0 +1,14 @@
+public class Nestedif {
+    public static void main(String[] args) {
+        String name = "Mohan";
+        int Roll = 25;
+        int marks = 85;
+        if (name == "Mohan" && Roll == 25) {
+            if (marks > 35) {
+                System.out.println("Mohan has been promoted to next class.");
+            } else {
+                System.out.println("Mohan needs to give re-exam.");
+            }
+        }
+    }
+}
